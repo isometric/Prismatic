a34a2a03079b74fe723849c00d23ce56073dd0ea
harism <harism@gmail.com>
2012-09-01 13:34:44 +0300

    Comments added.

--- src/fi/harism/cubism/CubismActivity.java
+++ src/fi/harism/cubism/CubismActivity.java
@@ -1,6 +1,23 @@
+/*
+   Copyright 2012 Harri Smatt
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 package fi.harism.cubism;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
@@ -17,12 +34,7 @@ public class CubismActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		// Force full screen view.
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		// getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-		// getWindow().clearFlags(
-		// WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
 
 		// Create GLSurfaceView.
 		mRenderer = new CubismRenderer(this);
@@ -33,15 +45,22 @@ public class CubismActivity extends Activity {
 		setContentView(mGLSurfaceView);
 		queueGLEvent();
 
+		// Create media player object.
 		mMediaPlayer = MediaPlayer.create(this, R.raw.loop);
 		mMediaPlayer.setLooping(true);
 		mMediaPlayer
 				.setOnSeekCompleteListener(new MediaPlayer.OnSeekCompleteListener() {
 					@Override
 					public void onSeekComplete(MediaPlayer arg0) {
+						// Tell renderer music is looping from the start.
 						mRenderer.onMusicRepeat();
 					}
 				});
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
 	}
 
 	@Override
@@ -66,10 +85,16 @@ public class CubismActivity extends Activity {
 		mMediaPlayer.start();
 	}
 
+	/**
+	 * Triggers runnable in rendering thread to be spawned.
+	 */
 	public void queueGLEvent() {
 		mGLSurfaceView.queueEvent(mGLQueueRunnable);
 	}
 
+	/**
+	 * Private timer for fading in the music.
+	 */
 	private class FadeInTimer extends CountDownTimer {
 
 		private long mMillisInFuture;
@@ -93,9 +118,13 @@ public class CubismActivity extends Activity {
 
 	}
 
+	/**
+	 * Private class for handling render thread events.
+	 */
 	private class GLQueueRunnable implements Runnable {
 		@Override
 		public void run() {
+			// Notify renderer.
 			mRenderer.onGLEvent();
 		}
 	}
